f0dceb0bb2c2adf14e3c6c82381bb8b8283a827e
redboo <garda1@mail.ru>
2022-11-17 21:14:56 +0500

    refactor: исправления предложенные новым линтером

--- tgbot_simple/bot.py
+++ tgbot_simple/bot.py
@@ -1,21 +1,32 @@
+"""Шаблон простого Telegram бота."""
 from aiogram import Bot, Dispatcher, executor, types
 from environs import Env
 
 env = Env()
 env.read_env()
-bot = Bot(token=env.str("BOT_TOKEN"))
+bot = Bot(token=env.str('BOT_TOKEN'))
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=["start", "help"])
-async def send_welcome(message: types.Message):
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message) -> None:
+    """Обработчик команд `/start` и `/help`.
+
+    Args:
+        message (types.Message): объект сообщения
+    """
     await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
 
 
 @dp.message_handler()
-async def echo(message: types.Message):
+async def echo(message: types.Message) -> None:
+    """Эхо. Повторяет текст, который вводит пользователь.
+
+    Args:
+        message (types.Message): объект сообщения
+    """
     await message.answer(message.text)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
